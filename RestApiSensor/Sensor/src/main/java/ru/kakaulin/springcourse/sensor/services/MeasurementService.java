c2f947b24983b35990af1df8b884f9bfe2f05c21
ank41 <104667714+ank41@users.noreply.github.com>
2022-11-26 15:34:32 +0700

    Update MeasurementService.java

--- RestApiSensor/Sensor/src/main/java/ru/kakaulin/springcourse/sensor/services/MeasurementService.java
+++ RestApiSensor/Sensor/src/main/java/ru/kakaulin/springcourse/sensor/services/MeasurementService.java
@@ -33,7 +33,7 @@ public class MeasurementService {
     }
 
     public void enrichMeasurement(Measurement measurement) {
-        // мы должны сами найти сенсор из БД по имени и вставить объект из Hibernate persistence context'а
+       
         measurement.setSensor(sensorService.findByName(measurement.getSensor().getName()).get());
 
         measurement.setMeasurementDateTime(LocalDateTime.now());
