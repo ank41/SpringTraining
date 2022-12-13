d85476c344da0346bfff57c3af877a8eb473eb5d
ank41 <104667714+ank41@users.noreply.github.com>
2022-12-13 14:28:20 +0700

    Delete financialApp directory

--- financialApp/src/main/java/ru/kakaulin/financialApp/util/accountUtils/AccountException.java
+++
@@ -1,7 +0,0 @@
-package ru.kakaulin.financialApp.util.accountUtils;
-
-public class AccountException  extends RuntimeException {
-    public AccountException(String msg) {
-        super(msg);
-}
-}
